b9c2e738675544f28055b2ead141560a89bc1d53
Keith Pounds <keith.pounds@autozone.com>
2018-07-11 14:27:55 -0500

    initial commit

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+// function to interpolate data between a template and data of object. 
+// Regex matches mustache formatted values in the template and replaces them with the matching data.
+function interpolateData(template, data) {
+  let rendered = template;
+  Object.keys(data).forEach(key => {
+    const regex = new RegExp(`{{\\s*${key}\\s*}}`, 'g');
+    rendered = rendered.replace(
+      regex,
+      typeof data[key] === 'undefined' ? '' : data[key]
+    );
+  });
+  return rendered;
+}
