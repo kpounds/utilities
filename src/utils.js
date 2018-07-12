21e09a25a4d3e7dc200e1d4bdc736bde0af774a3
Keith Pounds <keith.pounds@autozone.com>
2018-07-12 12:26:28 -0500

    updating timestap function

--- src/utils.js
+++ src/utils.js
@@ -17,24 +17,19 @@ function changeCSS(id, attribute, value) {
   document.getElementById(id).style[attribute] = value;
 };
 
-// gets Daylight Savings time offset
-function dstOffset() {
+// TimeStamp... 
+// convert and clean up string for UTC time to Central time 
+// takes parameter of 5 or 6 depending on Daylight savings time
+// can use dstOffset function
+function timeStampInCDT() {
+  let timelagging = 5; // for DST
   const now = new Date().getFullYear();
   const jan = new Date(now, 0, 1);
   const jul = new Date(now, 6, 1);
   const stdOffset = Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
   if (new Date().getTimezoneOffset() < stdOffset) {
-    return 6;
+    timelagging = 6;
   }
-  return 5;
-};
-
-// TimeStamp... 
-// convert and clean up string for UTC time to Central time 
-// takes parameter of 5 or 6 depending on Daylight savings time
-// can use dstOffset function
-function timeStampInCDT(dst) {
-  const timelagging = dst; // 5 or 6 for DST
   const utc = new Date();
   const cdt = new Date(utc.getTime() - 1 * 60 * 60 * 1000 * timelagging)
     .toISOString()
