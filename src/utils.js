1a9a00b8f588c1a51a706b7ae6475381b57a9cb1
Keith Pounds <keith.pounds@autozone.com>
2018-07-12 12:16:38 -0500

    adding and updating timestamp utils

--- src/utils.js
+++ src/utils.js
@@ -17,17 +17,31 @@ function changeCSS(id, attribute, value) {
   document.getElementById(id).style[attribute] = value;
 };
 
+// gets Daylight Savings time offset
+function dstOffset() {
+  const now = new Date().getFullYear();
+  const jan = new Date(now, 0, 1);
+  const jul = new Date(now, 6, 1);
+  const stdOffset = Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+  if (new Date().getTimezoneOffset() < stdOffset) {
+    return 6;
+  }
+  return 5;
+};
+
+// TimeStamp... 
 // convert and clean up string for UTC time to Central time 
 // takes parameter of 5 or 6 depending on Daylight savings time
-function convertUTCtoCDT(dst) {
-  const timelagging = dst; // 5 or 6 for Daylight Savings
+// can use dstOffset function
+function timeStampInCDT(dst) {
+  const timelagging = dst; // 5 or 6 for DST
   const utc = new Date();
   const cdt = new Date(utc.getTime() - 1 * 60 * 60 * 1000 * timelagging)
     .toISOString()
     .replace(/T/, ' ')
     .replace(/\..+/, '');
   return cdt;
-}
+};
 
 // create log file for NodeJS logging server side
 // need to import node util and fs to write to a file
